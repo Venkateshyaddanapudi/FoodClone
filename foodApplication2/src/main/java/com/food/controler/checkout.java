14953eba3a6cb86a2f90c08d2c9391eb9b84f2f5
Venkateshyaddanapudi <venkateshyaddanapudi341@gmail.com>
2025-01-04 08:01:29 +0530

    Add files via upload

---
+++ foodApplication2/src/main/java/com/food/controler/checkout.java
@@ -0,0 +1,168 @@
+package com.food.controler;
+
+import java.io.IOException;
+
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.foodapp.impl.OrderItemsDaoImpl;
+import com.foodapp.impl.OrdersDaoImpl;
+import com.foodapp.interfaces.OrderItemsDao;
+import com.foodapp.model.Cart;
+import com.foodapp.model.CartItem;
+import com.foodapp.model.OrderItems;
+import com.foodapp.model.Orders;
+import com.foodapp.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/checkout")
+public class checkout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private OrdersDaoImpl OrdersDao;
+	private OrderItemsDaoImpl OrderItems;
+
+
+
+	public void init() {
+
+		try {
+			OrdersDao=new OrdersDaoImpl();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void in() {
+		
+		try {
+			
+			OrderItems=new OrderItemsDaoImpl();
+				
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+
+
+		HttpSession session=req.getSession();
+
+		Cart cart=(Cart) session.getAttribute("cart");
+		User user=(User) session.getAttribute("u");
+
+//		System.out.println(cart);
+//		System.out.println(user);
+
+
+		if(cart !=null && user !=null && !cart.getItems().isEmpty()) {
+
+
+			//Extract checkout from data
+			String paymentMethod= req.getParameter("PaymentMethod");
+			System.out.println( paymentMethod);
+//			create and populate order object
+
+			Orders orders=new Orders();
+			orders.setUserId(user.getUserId());
+			System.out.println(user.getUserId());
+
+
+			String rd =(String)session.getAttribute("rid");
+			int rid=Integer.parseInt(rd );
+			System.out.println(rid);
+			orders.setRestaurantId(rid);
+			
+			
+//			Add cart items to the order and calculate total amount
+
+			int totalAmount =0;
+			for(CartItem item:cart.getItems().values()) {
+				totalAmount +=item.getPrice() *item.getQuantity();
+			}
+			orders.setTotalAmount(totalAmount);
+			System.out.println(totalAmount);
+			orders.setStatus("Pending");
+			orders.setPaymentMode(paymentMethod);
+			System.out.println(paymentMethod);
+
+
+// save the order to the database
+			int orderId = OrdersDao.insert(orders);
+			
+			System.out.println("HI");
+			
+			
+			
+			
+			
+			
+			
+			/*OrderItems orderitem=new OrderItems();
+			int orderid=orders.getOrderId();
+			int menuId=cart.getItems().values().iterator().next().getMenu_id();
+			int quantity=cart.getItems().values().iterator().next().getQuantity();
+			int itemtotal=(int)session.getAttribute("total");
+			orderitem.setOrderId(orderid);
+			System.out.println(orderid);
+			orderitem.setMenuId(menuId);
+			System.out.println(menuId);
+			orderitem.setQuantity(quantity);
+			System.out.println(quantity);
+			orderitem.setItemTotal(itemtotal);
+			System.out.println(itemtotal);
+			OrderItemsDao orderdaoimpl=new OrderItemsDaoImpl ();
+			orderdaoimpl.insert(orderitem);*/
+			
+			int itemTotalAmount =0;
+			int x=0;
+			OrderItems orderitem=new OrderItems();
+			OrderItemsDao orderitems=new OrderItemsDaoImpl ();
+			
+			if(cart!=null && !cart.getItems().isEmpty()) {
+				
+					Set<Entry<Integer,CartItem>> entrySet = cart.getItems().entrySet();
+					
+					for(Entry<Integer,CartItem> entry : entrySet) {
+						itemTotalAmount= entry.getValue().getQuantity() * entry.getValue().getPrice();
+						x=orderitems.insert(new OrderItems (orderId,entry.getValue().getMenu_id(),entry.getValue().getQuantity(),itemTotalAmount));
+						//totalAmount=0;
+						
+					}
+					if(x!=0) {
+						resp.sendRedirect("orderSuccess.jsp");
+					}
+					else {
+						resp.sendRedirect("orderFailure.jsp");
+					}
+			}
+
+
+		}
+
+		else {
+			resp.sendRedirect("home.jsp");
+		}
+
+
+
+
+	}
+
+
+}
---
+++ foodApplication2/src/main/java/com/foodapp/interfaces/UserDao.java
@@ -0,0 +1,17 @@
+package com.foodapp.interfaces;
+
+import java.util.ArrayList;
+
+
+import com.foodapp.model.User;
+
+public interface UserDao {
+	
+	int insert(User u);
+	ArrayList<User>  fetchAll();
+	User fetchOne(int id);
+	int update(int id,String username);
+	int delete(int id);
+	User fetch(String email);
+
+}
